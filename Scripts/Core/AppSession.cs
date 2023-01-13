aea75b8f159a22d931346f35a8d87115bf4cb37d
Vadzim Kotovich <gergon1999@gmail.com>
2023-01-13 20:03:20 +0300

    [DEV] add first test 😊

--- Scripts/Core/AppSession.cs
+++ Scripts/Core/AppSession.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections.Generic;
-using Ji2Core.Core;
+﻿using Ji2Core.Core;
 using Ji2Core.Core.States;
-using Newtonsoft.Json;
-using UnityEngine;
 
 namespace Ji2Core.Core
 {
@@ -15,44 +12,6 @@ namespace Ji2Core.Core
         public AppSession(StateMachine stateStateMachine)
         {
             StateMachine = stateStateMachine;
-        }
-    }
-
-    public class PlayerPrefsSaveDataContainer : ISaveDataContainer
-    {
-        private const string SAVE_FILE_KEY = "SaveData";
-
-        private Dictionary<string, string> saves;
-
-        public void Load()
-        {
-            if (PlayerPrefs.HasKey(SAVE_FILE_KEY))
-            {
-                var savesText = PlayerPrefs.GetString(SAVE_FILE_KEY);
-                saves = JsonConvert.DeserializeObject<Dictionary<string, string>>(savesText);
-            }
-            else
-            {
-                saves = new Dictionary<string, string>();
-            }
-        }
-
-        public void Save()
-        {
-            var text = JsonConvert.SerializeObject(saves);
-            PlayerPrefs.SetString(SAVE_FILE_KEY, text);
-            Debug.LogError(text);
-        }
-
-        public void SaveValue(string key, object value)
-        {
-            saves.Add(key, JsonConvert.SerializeObject(value));
-            Save();
-        }
-
-        public T GetSave<T>(string key)
-        {
-            return JsonConvert.DeserializeObject<T>(saves[key]);
         }
     }
 
---
+++ Scripts/Core/PlayerPrefsSaveDataContainer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace Ji2Core.Core
+{
+    public class PlayerPrefsSaveDataContainer : ISaveDataContainer
+    {
+        private const string SAVE_FILE_KEY = "SaveData";
+
+        private Dictionary<string, string> saves;
+
+        public void Load()
+        {
+            if (PlayerPrefs.HasKey(SAVE_FILE_KEY))
+            {
+                var savesText = PlayerPrefs.GetString(SAVE_FILE_KEY);
+                saves = JsonConvert.DeserializeObject<Dictionary<string, string>>(savesText);
+            }
+            else
+            {
+                saves = new Dictionary<string, string>();
+            }
+        }
+
+        public void Save()
+        {
+            var text = JsonConvert.SerializeObject(saves);
+            PlayerPrefs.SetString(SAVE_FILE_KEY, text);
+        }
+
+        public void SaveValue(string key, object value)
+        {
+            saves[key] = JsonConvert.SerializeObject(value);
+            Save();
+        }
+
+        public T LoadValue<T>(string key)
+        {
+            return JsonConvert.DeserializeObject<T>(saves[key]);
+        }
+    }
+}
