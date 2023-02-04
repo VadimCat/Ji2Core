e604ce2a63c7d0bd3e44b8fc8636d7f16dbe7894
Vadzim Kotovich <gergon1999@gmail.com>
2023-02-04 14:52:20 +0300

    add grid spacing tween

---
+++ Scripts/Utils/GridTweenExtension.cs
@@ -0,0 +1,14 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ji2Core.Utils
+{
+    public static class GridTweenExtension
+    {
+        public static Tween DoSpacing(this GridLayoutGroup grid, Vector2 target, float time)
+        {
+            return DOTween.To(() => grid.spacing, spacing => grid.spacing = spacing, Vector2.zero, .2f);
+        }
+    }
+}
