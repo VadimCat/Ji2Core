d8f2039639b1c7753c6af852a90f9755c4eb62c3
ialexofficial <ialexofficial@mail.ru>
2023-09-14 00:03:46 +0300

    Added TimeScaler, LayerMasker, CameraConstantFit tools and DragInput

---
+++ Scripts/Core/Tools/LayerMasker.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Ji2Core.Core.Tools
+{
+    public static class LayerMasker
+    {
+        public static bool CheckLayer(LayerMask mask, int layer) =>
+            (mask.value & (1 << layer)) != 0;
+
+        public static int MergeLayerMasks(params LayerMask[] masks) =>
+            masks.Aggregate((mask1, mask2) => mask1.value | mask2.value);
+
+        public static int SubtractLayersFromMask(LayerMask layerMask, params int[] layers) =>
+            layers.Aggregate(layerMask.value, (result, layer) =>
+                result & ~(1 << layer)
+            );
+    }
+}
